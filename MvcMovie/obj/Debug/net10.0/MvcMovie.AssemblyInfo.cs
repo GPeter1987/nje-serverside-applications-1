cc575c0ef4f39dd8ba90a1e5d765dee13d090ef2
Gaál Péter <33869458+GPeter1987@users.noreply.github.com>
2026-09-19 20:30:52 +0200

    Add a model

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992f47965befa5a027bf136e865b854eed4d5a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30a40c7a15010d13ffe8968eb30a5f1ed82d498")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
