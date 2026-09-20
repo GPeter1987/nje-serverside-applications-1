ca11e4b8abd0908d7b5faaad298c4bfb5ba220a0
Gaál Péter <33869458+GPeter1987@users.noreply.github.com>
2026-09-20 12:32:26 +0200

    Add validation

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0edcc55442ab9a35062cda6fd2161d8c07574ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11beb2fdf3dbd7dc08ee843eb443081b4e48850a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
