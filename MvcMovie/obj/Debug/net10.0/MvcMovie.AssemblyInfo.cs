84daf3a92731c9d85b87b0f55c28becac10004e8
Gaál Péter <33869458+GPeter1987@users.noreply.github.com>
2026-09-20 10:32:17 +0200

    Controller actions and views

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc575c0ef4f39dd8ba90a1e5d765dee13d090ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b91ed06ffb50ee6cf3929d5576a24242c3852f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
